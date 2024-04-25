5cde4130e259e365c5affc093797a6f20087efa6
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-04-25 22:54:11 +0530

    Created a form for adding grocery

--- lib/screens/new_item.dart
+++ lib/screens/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_track/data/categories.dart';
 
 class NewItemScreen extends StatefulWidget {
   const NewItemScreen({super.key});
@@ -11,6 +12,76 @@ class NewItemScreen extends StatefulWidget {
 class _NewItemScreenState extends State<NewItemScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add New Grocery"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Form(
+            child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                label: Text('Name'),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      label: Text('Quantity'),
+                    ),
+                    initialValue: '1',
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: DropdownButtonFormField(
+                    items: [
+                      for (final category in categories.entries)
+                        DropdownMenuItem(
+                          value: category.value,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 16,
+                                height: 16,
+                                color: category.value.color,
+                              ),
+                              const SizedBox(width: 8),
+                              Text(
+                                category.value.title,
+                              ),
+                            ],
+                          ),
+                        ),
+                    ],
+                    onChanged: (value) {},
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 12),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('Reset'),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Add Grocery'),
+                )
+              ],
+            )
+          ],
+        )),
+      ),
+    );
   }
 }

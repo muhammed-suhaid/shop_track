e58f894a86b717a224ea3380aef6157c2df0beab
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-04-25 22:26:01 +0530

    created new item screen and appbar for grocery list screen which has actions that connects with add item screen

--- lib/screens/grocery_list.dart
+++ lib/screens/grocery_list.dart
@@ -1,25 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:shop_track/data/dummy_items.dart';
+import 'package:shop_track/screens/new_item.dart';
 
-class GroceryListScreen extends StatelessWidget {
+class GroceryListScreen extends StatefulWidget {
   const GroceryListScreen({super.key});
+
+  @override
+  State<GroceryListScreen> createState() => _GroceryListScreenState();
+}
+
+class _GroceryListScreenState extends State<GroceryListScreen> {
+  void _addItem() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const NewItemScreen(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: groceryItems.length,
-        itemBuilder: (context, index) => ListTile(
-          title: Text(
-            groceryItems[index].name,
+      appBar: AppBar(
+          title: const Text(
+            'Your Groceries',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-          leading: Container(
-            width: 24,
-            height: 24,
-            color: groceryItems[index].category.color,
-          ),
-          trailing: Text(
-            groceryItems[index].quantity.toString(),
+          actions: [
+            IconButton(
+              onPressed: _addItem,
+              icon: const Icon(Icons.add),
+            ),
+          ]),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: ListView.builder(
+          itemCount: groceryItems.length,
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemBuilder: (context, index) => ListTile(
+            title: Text(
+              groceryItems[index].name,
+            ),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: groceryItems[index].category.color,
+            ),
+            trailing: Text(
+              groceryItems[index].quantity.toString(),
+            ),
           ),
         ),
       ),
---
+++ lib/screens/new_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NewItemScreen extends StatefulWidget {
+  const NewItemScreen({super.key});
+  @override
+  State<NewItemScreen> createState() {
+    return _NewItemScreenState();
+  }
+}
+
+class _NewItemScreenState extends State<NewItemScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
